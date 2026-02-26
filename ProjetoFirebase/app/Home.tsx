190d3278b4b5336d2bebd000a72e8e71c816c8a4
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-02-26 11:47:19 -0300

    excluir conta

--- ProjetoFirebase/app/Home.tsx
+++ ProjetoFirebase/app/Home.tsx
@@ -1,6 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, Alert } from "react-native";
+import { auth } from "../services/firebaseConfig";
+import { deleteUser } from "firebase/auth";
 
 export default function Home() {
   const router = useRouter(); // Hook de navegacao
@@ -9,10 +11,41 @@ export default function Home() {
     await AsyncStorage.removeItem("@user"); // Limpa o usuario do Async
     router.replace("/"); // Redireciona para a tela de login
   };
+
+  const excluirConta = () => {
+    Alert.alert(
+      "Confirmar Exclusao",
+      "Tem certeza que deseja excluir sua conta?",
+      [
+        { text: "Cancelar", style: "cancel" },
+        {
+          text: "Excluir",
+          onPress: async () => {
+            try{
+              const user = auth.currentUser
+              if(user) {
+                await deleteUser(user)
+                await AsyncStorage.removeItem("@user")
+                Alert.alert("Sucesso", "Conta excluida!")
+                router.replace("/")
+              } else {
+                Alert.alert("Erro", "Nenhum usuario logado.")
+              }
+            } catch(error) {
+              console.log("Erro ao excluir conta.")
+              Alert.alert("Erro", "Nao foi possivel excluir a conta")
+            }
+          },
+        },
+      ]
+    );
+  };
+
   return (
     <View style={styles.main}>
       <Text>Tela Home</Text>
       <Button title="Realizar logoff" onPress={realizarLogoff} />
+      <Button title="Excluir conta" color="red" onPress={excluirConta} />
     </View>
   );
 }
